448d5d0135d8f4bb10e4d4bb52470416b4f0e674
Moo367 <m.ali.ambacht@gmail.com>
2024-03-28 14:08:14 +0100

    verder gewerkt

--- index.php
+++ index.php
@@ -1,1 +1,50 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Uneed-It</title>
+</head>
+<body>
+
+<img src="uneed%20it%20logo.png" class="Logo"/>
+<nav class="navbar">
+    <a href="Home.php">Home</a>
+    <a href="Over ons">Over ons</a>
+    <a href="Webshop">Webshop</a>
+    <a href="Zakelijk">Zakelijk</a>
+    <a href="Service">Service</a>
+    <a href="Reparatie's">Reparatie's</a>
+    <a href="It Nieuws">About</a>
+    <section>
+        <h2>Lorem ipsum</h2>
+        <p>✓Lorem ipsum</p>
+        <p>✓Lorem ipsum</p>
+        <p>✓Lorem ipsum</p>
+        <p>✓Lorem ipsum</p>
+    </section>
+    <section>
+        <p>Home Over Ons Webshop Zakelijk Service Reparaties IT Nieuws</p>
+        <p>U HET GEBRUIK, WIJ DE ZORG!</p>
+        <h3>Leer ons kennen</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+    </section>
+    <footer>
+        <div id="links">
+        <p>ZUIDBAAN 514, 2841MD</p>
+        <p>MOORDRECHT</p>
+        </div>
+        <div id="midden">
+        <p>UNEED - IT Lorem ipsum</p>
+        <p>ZUIDBAAN 514, 2841MD MOORDRECHT</p>
+        </div
+        <div id="rechts">
+        <p>+316 30 985 409 (service nummer)</p>
+        <p>+3118 28 202 18 (KANTOOR, BEREIKBAAR VAN 09:00-18:00)</p>
+        </div>
+    </footer>
+</nav>
+</body>
+</html>
+
