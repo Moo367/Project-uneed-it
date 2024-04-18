9676c1b9cb5c5aa8319274241ef0abcc771feaa7
Julio Tamaela <juliotamaela774@gmail.com>
2024-04-18 12:07:41 +0200

    ja

---
+++ It-nieuws.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>itnieuws</title>
+    <link rel="stylesheet" href="stylesheeter.css">
+</head>
+<body>
+<div class="navbar">
+    <a class="active" href="#"><i class="fa fa-fw fa-home"></i>Home</a>
+    <a href="index.php"><i class="fa fa-fw fa-users"></i>Over ons</a>
+    <a href="#"><i class="fa fa-fw fa-cart-shopping"></i>Webshop</a>
+    <a href="#"><i class="fa fa-fw fa-briefcase"></i>Zakelijk</a>
+    <a href="#"><i class="fa fa-fw fa-user"></i>Service</a>
+    <a href="reparaties.php"><i class="fa fa-fw fa-laptop"></i>Reparaties</a>
+    <a href="#"><i class="fa fa-fw fa-newspaper"></i>It Nieuws</a>
+    <img id="logonav" src="uneed%20it%20logo.png"/>
+
+</div>
+
+<img id="foto1" src="uneedit02.jpg">
+        <div class="lorem">
+        <footer id="topfooter">
+        <div id="links">
+        <p>ZUIDBAAN 514, 2841MD</p>
+        <p>MOORDRECHT</p>
+        </div>
+        <div id="midden">
+            <p>asdadad</p>
+            <p>asdadad</p>
+            <p>asdadad</p>
+        </div>
+        <div id="rechts">
+        <p>+316 30 985 409 (service nummer)</p>
+        <p>+3118 28 202 18 <br> (KANTOOR, BEREIKBAAR VAN 09:00-18:00)</p>
+        </div>
+    </footer>
+            <h1>Lorem Ipsum</h5>
+       <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit pariatur distinctio amet dolorum maiores dicta facere, culpa explicabo laboriosam, temporibus, saepe rem laudantium dolore? Eos commodi sit nobis ipsum velit?Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias saepe quis nam iure molestiae eos adipisci possimus, illum ullam enim odio voluptatum assumenda repellendus mollitia quod magnam dolorem quia voluptates! Lorem ipsum dolor sit, amet consectetur adipisicing elit. Inventore adipisci aliquid officiis, eligendi nam commodi itaque quidem aliquam ab. Cum, at eaque eos corrupti illo a placeat veniam. Adipisci, explicabo. Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum dignissimos repudiandae magni natus quos consequatur vero, soluta deserunt itaque repellendus cum rem porro sint dolorem delectus quis sapiente earum modi.</h3>
+        </div>
+        
+<header>
+    <h1>Laatste Nieuws</h1>
+</header>
+
+<div class="container22">
+    <article class="a1">
+            <img src="images.jpg" alt="" class="images">
+        <h2>Breaking News: XYZ</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed suscipit urna sit amet justo pulvinar, vitae efficitur neque interdum. In hac habitasse platea dictumst.</p>
+        <p><a href="#">Read more</a></p>
+    </article>
+    
+    <article class="a2">
+    <img src="images.jpg" alt="" class="images">
+        <h2>Update on ABC Situation</h2>
+        <p>Phasellus vitae lacus eget lacus hendrerit fermentum eget eget ex. Duis sit amet sapien sed lectus congue facilisis. Proin sit amet malesuada velit.</p>
+        <p><a href="#">Read more</a></p>
+    </article>
+    
+    <article>
+    <img src="images.jpg" alt="" class="images">
+        <h2>New Study Reveals Insights</h2>
+        <p>Donec commodo vestibulum ex, vel pellentesque dui gravida at. Morbi venenatis risus nec commodo vestibulum. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>
+        <p><a href="#">Read more</a></p>
+    </article>
+</div>
+<div class="container22">
+    <article class="a1">
+    <img src="images.jpg" alt="" class="images">
+        <h2>Breaking News: XYZ</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed suscipit urna sit amet justo pulvinar, vitae efficitur neque interdum. In hac habitasse platea dictumst.</p>
+        <p><a href="#">Read more</a></p>
+    </article>
+    
+    <article class="a2">
+        <img src="images.jpg" alt="" class="images">
+        <h2>Update on ABC Situation</h2>
+        <p>Phasellus vitae lacus eget lacus hendrerit fermentum eget eget ex. Duis sit amet sapien sed lectus congue facilisis. Proin sit amet malesuada velit.</p>
+        <p><a href="#">Read more</a></p>
+    </article>
+    
+    <article>
+        <img src="images.jpg" alt="" class="images">
+        <h2>New Study Reveals Insights</h2>
+        <p>Donec commodo vestibulum ex, vel pellentesque dui gravida at. Morbi venenatis risus nec commodo vestibulum. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>
+        <p><a href="#">Read more</a></p>
+    </article>
+</div>
+
+</body>
+</html>
--- reparaties.php
+++ reparaties.php
@@ -4,21 +4,17 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Selectielijst</title>
-    <script src="https://kit.fontawesome.com/7b49be9210.js" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="style.css">
-    <link rel="stylesheet" href="reparatiescss.css">
-</head>
-<body>
-<div class="navbar">
-    <a href="index.php"><i class="fa fa-fw fa-home"></i>Home</a>
-    <a href="Over%20Ons.php"><i class="fa fa-fw fa-users"></i>Over ons</a>
-    <a href="webshop.php"><i class="fa fa-fw fa-cart-shopping"></i>Webshop</a>
-    <a href="zakelijk.php"><i class="fa fa-fw fa-briefcase"></i>Zakelijk</a>
-    <a href="service.php"><i class="fa fa-fw fa-user"></i>Service</a>
-    <a class="active" href="reparaties.php"><i class="fa fa-fw fa-laptop"></i>Reparaties</a>
+    <link rel="Stylesheet" href="stylesheeter.css">
+    </head>
+    <body>
+    <div class="navbar">
+    <a class="active" href="#"><i class="fa fa-fw fa-home"></i>Home</a>
+    <a href="index.php"><i class="fa fa-fw fa-users"></i>Over ons</a>
+    <a href="#"><i class="fa fa-fw fa-cart-shopping"></i>Webshop</a>
+    <a href="#"><i class="fa fa-fw fa-briefcase"></i>Zakelijk</a>
+    <a href="#"><i class="fa fa-fw fa-user"></i>Service</a>
+    <a href="#"><i class="fa fa-fw fa-laptop"></i>Reparaties</a>
     <a href="It-nieuws.php"><i class="fa fa-fw fa-newspaper"></i>It Nieuws</a>
-    <a href="faq.php"><i class="fa-solid fa-question"></i>FAQ</a>
-    <a href="login.php"><i class="login"></i>Login</a>
     <img id="logonav" src="uneed%20it%20logo.png"/>
 
 </div>
@@ -36,41 +32,41 @@
 
             <label for="phone">Telefoonnummer:</label>
             <input type="tel" id="phone" name="phone" required>
-
-            <label for="appliance">Apparaat:</label>
-            <select id="appliance" name="appliance" required>
-                <option value="">Selecteer een apparaat</option>
-                <option value="Apple">Apple</option>
-                <option value="Samsung">Samsung</option>
-                <option value="Huawei">Huawei</option>
-                <option value="Acer">Vaatwasser</option>
-            </select>
-
-            <label for="device">Device:</label>
-            <input type="text" id="device" name="device" required>
-
-            <label for="issue">Probleemomschrijving:</label>
-            <textarea id="issue" name="issue" rows="4" required></textarea>
-
-            <input type="submit" value="Verzend reparatieverzoek">
-        </form>
+              
+              <label for="Device">Device</label>
+              <input type="text" id="Device" name="Device" required>
+            
+              <label for="issue">Probleemomschrijving:</label>
+              <textarea id="issue" name="issue" rows="4" required></textarea>
+            
+              <input type="submit" value="Verzend reparatieverzoek">
+            </form>
+        </div>
+        <div class="lorem2">
+          <H2>Lorem ipsum</H2>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio sunt id quis facere assumenda! Aspernatur, commodi accusantium blanditiis possimus nobis suscipit maxime, tempora nemo beatae obcaecati rerum cupiditate ut inventore! Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora aliquam quis quae eligendi, est recusandae praesentium officiis repudiandae quam veritatis consequuntur animi enim obcaecati perspiciatis ratione, quia quidem optio rerum?</p>
+        </div>
+        
+        <div class="footer1">
+        <h3 class="footer-socials">Services</h3>
+        <div class="services1">
+            <ul>
+                <a class="footer-socials ul1" href="#">Reparaties</a></li>
+                <a class="footer-socials ul1" href="#">Webshop</a></li>
+                <a class="footer-socials ul1" href="#">FAQ</a></li>
+            </ul>
+            <img class="footer-logo" src="DHL.png">
+            <img class="footer-logo homerr" src="homerr.png">
+            <img class="footer-logo" src="ups%20logo.png">
+        </div>
     </div>
-</main>
-
-<footer id="topfooter">
-    <div id="links">
-        <p>ZUIDBAAN 514, 2841MD</p>
-        <p>MOORDRECHT</p>
+    <h3 class="socials-div">About</h3>
+    <div class="socials-div">
+        <a class="socials-text" href="#">Twitter</a>
+        <a class="socials-text" href="#">Facebook</a>
+        <a class="socials-text" href="#">Linkedin</a>
+        <a class="socials-text" href="#">Instagram</a>
     </div>
-    <div id="midden">
-        <p>asdadad</p>
-        <p>asdadad</p>
-        <p>asdadad</p>
-    </div>
-    <div id="rechts">
-        <p>+316 30 985 409 (service nummer)</p>
-        <p>+3118 28 202 18 <br> (KANTOOR, BEREIKBAAR VAN 09:00-18:00)</p>
-    </div>
-</footer>
-</body>
-</html>
+</div>
+    </body>
+    </html>
